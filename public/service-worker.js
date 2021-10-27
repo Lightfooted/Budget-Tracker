d36216d8e3eabcad506e1f200a507055c28d1180
Lightfooted <vesselofbalance@hotmail.com>
2021-10-27 00:38:00 -0400

    changed server worker to what was taught in activities. still having same issues

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
-const APP_PREFIX = 'Tracker-';     
-const VERSION = 'version_01';
-const CACHE_NAME = APP_PREFIX + VERSION;
+const CACHE_NAME = 'budget-cache-v1';
+const DATA_CACHE_NAME = 'data-cache-v1';
+
 const FILES_TO_CACHE = [
   "/",
   "./index.html",
@@ -20,49 +20,68 @@ const FILES_TO_CACHE = [
 
 // Install service worker
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            console.log('installing cache : ' + CACHE_NAME);
-            return cache.addAll(FILES_TO_CACHE);
-        })
-    );
-    self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Your files were pre-cached successfully!');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
 });
 
 // Delete old data from caches
-self.addEventListener('activate', function (event) {
+self.addEventListener('activate', function(event) {
   event.waitUntil(
-    caches.keys().then(function (keyList) {
-      let cacheKeeplist = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX);
-      })
-
-      cacheKeeplist.push(CACHE_NAME);
-
-      return Promise.all(keyList.map(function (key, i) {
-        if (cacheKeeplist.indexOf(key) === -1) {
-          console.log('deleting cache : ' + keyList[i] );
-          return caches.delete(keyList[i]);
-        }
-      }));
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log('Removing old cache data', key);
+            return caches.delete(key);
+          }
+        })
+      );
     })
   );
+
   self.clients.claim();
 });
 
 // Fetch request
-self.addEventListener('fetch', function (event) {
-    console.log('fetch request : ' + event.request.url)
+self.addEventListener('fetch', function(event) {
+  if (event.request.url.includes('/api/')) {
     event.respondWith(
-      caches.match(e.request).then(function (request) {
-        if (request) { // if cache is available, respond with cache
-          console.log('responding with cache : ' + event.request.url)
-          return request
-        } else {       // if there are no cache, try fetching request
-          console.log('file is not cached, fetching : ' + event.request.url)
+      caches
+        .open(DATA_CACHE_NAME)
+        .then(cache => {
           return fetch(event.request)
+            .then(response => {
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch(err => {
+              return cache.match(event.request);
+            });
+        })
+        .catch(err => console.log(err))
+    );
+
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get('accept').includes('text/html')) {
+          return caches.match('/');
         }
-  
-      })
-    )
-  })
+      });
+    })
+  );
+});
