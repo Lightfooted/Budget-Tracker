f630fea24b97af172da4dabc1e871002ac53eae4
Lightfooted <vesselofbalance@hotmail.com>
2021-10-27 00:39:04 -0400

    uninstalled old version of mongoose and updated to newest version. removed deprecated code from server

--- server.js
+++ server.js
@@ -17,9 +17,6 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
 });
 
 // routes
